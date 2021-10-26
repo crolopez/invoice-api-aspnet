7cc613b4f66772bd4c942e318c680fb2fb09f762
Cristobal Lopez <crolopezp@gmail.com>
2021-10-26 11:19:54 -0700

    Create invoice model

---
+++ Models/Invoice.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace InvoiceApi.Models
+{
+    public class Invoice
+    {
+        public long InvoiceId { get; set; }
+        public string Supplier { get; set; }
+        public DateTime DateIssued { get; set; }
+        public string Currency { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; }
+}
