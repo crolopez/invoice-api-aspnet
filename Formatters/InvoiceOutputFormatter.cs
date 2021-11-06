80df32b475957565423ad3176188ca7f05992193
Cristobal Lopez <crolopezp@gmail.com>
2021-11-06 06:02:11 -0700

    Ignore null field in JSON responses

--- Formatters/InvoiceOutputFormatter.cs
+++ Formatters/InvoiceOutputFormatter.cs
@@ -6,18 +6,25 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace InvoiceApi.Formatters {
     public class InvoiceOutputFormatter : TextOutputFormatter
     {
+        private readonly JsonSerializerOptions _jsonSettings;
+
         public InvoiceOutputFormatter()
         {
             SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/json"));
             SupportedEncodings.Add(Encoding.UTF8);
             SupportedEncodings.Add(Encoding.Unicode);
+
+            _jsonSettings = new JsonSerializerOptions();
+            _jsonSettings.IgnoreNullValues = true;
         }
 
         protected override bool CanWriteType(Type type)
@@ -31,7 +38,7 @@ namespace InvoiceApi.Formatters {
         {
             var httpContext = context.HttpContext;
             var method = httpContext.Request.Method;
-            object response = new {};
+            Response<Invoice> response;
 
             switch (context.Object) {
                 case IEnumerable<Invoice>:
@@ -40,9 +47,12 @@ namespace InvoiceApi.Formatters {
                 case Invoice:
                     response = GetResponse(method, (Invoice) context.Object);
                 break;
+                default: // TODO
+                    return;
             }
 
-            await httpContext.Response.WriteAsJsonAsync(response);
+            var responseStream = httpContext.Response.Body;
+            await SerializeResponse(responseStream, response);
         }
 
         private Response<Invoice> GetResponse(string method, Invoice invoice)
@@ -60,6 +70,10 @@ namespace InvoiceApi.Formatters {
                 .ConvertAll<InvoiceDataNode>(invoice =>
                     new InvoiceDataNode(invoice));
             return new Response<Invoice>(invoiceList);
+        }
+
+        private Task SerializeResponse(Stream responseStream, Response<Invoice> response) {
+            return JsonSerializer.SerializeAsync(responseStream, response, _jsonSettings);
         }
     }
 }
