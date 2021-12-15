6ec74c61557f1ea298adb10e4f8949296c824919
Cristobal Lopez <crolopezp@gmail.com>
2021-12-15 15:35:06 -0800

    Rename config class

--- InvoiceApi/Core/Domain/Models/APIConfig.cs
+++ InvoiceApi/Core/Domain/Models/APIConfig.cs
@@ -3,5 +3,6 @@ namespace InvoiceApi.Core.Domain.Models
   public class APIConfig
   {
       public string CurrencyConverterKey { get; set; }
+      public string DatabaseConnectionString { get; set; }
   }
 }
